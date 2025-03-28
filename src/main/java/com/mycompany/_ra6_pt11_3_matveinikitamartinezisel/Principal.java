ba3cb07d70c2bcac47c1788750d5fe70b413d6c8
nikita <nikitamatpech@gmail.com>
2025-03-28 09:56:08 +0100

    Versió incial del programa

---
+++ src/main/java/com/mycompany/_ra6_pt11_3_matveinikitamartinezisel/Principal.java
@@ -0,0 +1,12 @@
+package com.mycompany._ra6_pt11_3_matveinikitamartinezisel;
+
+/**
+ *
+ * @author nikit
+ */
+public class Principal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
