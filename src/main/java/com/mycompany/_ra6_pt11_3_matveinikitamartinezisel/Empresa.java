a7111042f88ed9dfc2f4fde7a984aa0459b5b6ee
nikita <nikitamatpech@gmail.com>
2025-03-31 21:45:57 +0200

    Codi complert de la classe Treballador i casi complert de la classe Empresa.

--- src/main/java/com/mycompany/_ra6_pt11_3_matveinikitamartinezisel/Empresa.java
+++ src/main/java/com/mycompany/_ra6_pt11_3_matveinikitamartinezisel/Empresa.java
@@ -1,9 +1,72 @@
 package com.mycompany._ra6_pt11_3_matveinikitamartinezisel;
 
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Map;
+
 /**
+ * Classe empresa que conté treballadors
  *
- * @author nikit
+ * @author Nikita i Isel
  */
 public class Empresa {
+
+    /*Declaració d'atributs*/
+    private Map<String, Treballador> treballadors;
     
+    /**
+     * Constructor de la classe
+     */
+    public Empresa() {
+        this.treballadors = new HashMap<>();
+    }
+    
+    /**
+     * Mètode addTreballador per efegir un treballador amb la seva key(DNI) en el
+     * HashMap de treballadors.
+     * 
+     * @param dni
+     * @param treballador 
+     */
+    public void addTreballador(String dni, Treballador treballador){
+        /*Comprovem que el DNI que introduïm no es troba en ús*/
+        if (!treballadors.containsKey(dni)) {
+            treballadors.put(dni, treballador);
+            System.out.println("Treballador afegit correctament.");
+        } 
+        /*Si ho està, no el afegim*/
+        else {
+            System.out.println("ERROR: No pots afegir aquest trebellador, el "
+                    + "DNI ja es troba sota un altre treballador.");
+        }
+    }
+    
+    /**
+     * Mètode edatMitjanaTreballadors per poder treure l'edat mitjana dels 
+     * treballadors
+     * 
+     * @return l'edat mitjana dels treballadors
+     */
+    public double edatMitjanaTreballadors(){
+        /*Comprovem que la llista no estigui buida*/
+        if (treballadors.isEmpty()){ return 0; }
+        
+        /*Estructura FOR-EACH per passar per tots els treballadors i conseguir 
+        la seva edat*/
+        int sumaEdats = 0;
+        for (Treballador t : treballadors.values()) {
+            sumaEdats += t.getEdat();
+        }
+        
+        /*Declarem una variable per guardar l'edat mitjana*/
+        double mitjana = (double) sumaEdats / treballadors.size();
+        
+        /*Retornem l'edat mtijana*/
+        return mitjana;
+    }
+    /*
+    public void trobarTreballadorsPerAltura(double alturaMinima){
+        ArrayList <Treballador> llistaTreballadors = new ArrayList<>(treballadors.values());
+        
+    }*/
 }
--- src/main/java/com/mycompany/_ra6_pt11_3_matveinikitamartinezisel/Treballador.java
+++ src/main/java/com/mycompany/_ra6_pt11_3_matveinikitamartinezisel/Treballador.java
@@ -1,9 +1,103 @@
 package com.mycompany._ra6_pt11_3_matveinikitamartinezisel;
 
 /**
+ * Classe Treballador
  *
  * @author Nikita i Isel
  */
 public class Treballador {
-    
+
+    /*Declaració de variables i atribusts*/
+    private String nom;
+    private int edat;
+    private double altura;
+    private static final String NOM_DEFAULT = "";
+    private static final int EDAT_DEFAULT = 18;
+    private static final double ALTURA_DEFAULT = 1.75;
+
+    /**
+     * Constructor per defecte que truca al constructor parametritzat
+     */
+    public Treballador() {
+        this(NOM_DEFAULT, EDAT_DEFAULT, ALTURA_DEFAULT);
+    }
+
+    /**
+     * Constructor parametritzat
+     *
+     * @param nom
+     * @param edat
+     * @param altura
+     */
+    public Treballador(String nom, int edat, double altura) {
+        this.nom = nom;
+        this.edat = edat;
+        this.altura = altura;
+    }
+
+    /**
+     * Mètode GET per conseguir el nom del treballador
+     *
+     * @return nom del treballador
+     */
+    public String getNom() {
+        return nom;
+    }
+
+    /**
+     * Mètode GET per conseguir l'edat del treballador
+     *
+     * @return edat del treballador
+     */
+    public int getEdat() {
+        return edat;
+    }
+
+    /**
+     * Mètode GET per conseguir l'altura del treballador
+     *
+     * @return altura
+     */
+    public double getAltura() {
+        return altura;
+    }
+
+    /**
+     * Mètode SET per canviar el nom del treballador
+     *
+     * @param nom
+     */
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    /**
+     * Mètode SET per canviar l'edat del treballador
+     *
+     * @param edat
+     */
+    public void setEdat(int edat) {
+        this.edat = edat;
+    }
+
+    /**
+     * Mètode SET per canviar l'altura del treballador
+     *
+     * @param altura
+     */
+    public void setAltura(double altura) {
+        this.altura = altura;
+    }
+
+    /**
+     * Mètode sobrescrit toString per dir totes les dades del treballador
+     *
+     * @return tota la informació del treballador
+     */
+    @Override
+    public String toString() {
+        String informacioTreballador = "Informació del treballador:" + "\nNom: "
+                + nom + "\nEdat: " + edat + "\nAltura: " + altura;
+        return informacioTreballador;
+    }
 }
